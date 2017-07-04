0cc3eb67f85484e0f1038f7968af5e8004185399
Unknown <gerhard@abomate.com>
2017-07-04 15:29:58 +0200

    Async finctions do not work

--- BatteryProfile/battery-level-characteristic.js
+++ BatteryProfile/battery-level-characteristic.js
@@ -10,6 +10,8 @@
 // import utility library to build classes
 var util = require('util');
 
+var async = require('async');
+
 // import os library to test operating system
 var os = require('os');
 
@@ -24,6 +26,8 @@ var Descriptor = bleno.Descriptor;
 
 // create the Characteristic class which the battery level characteristic inherits from
 var Characteristic = bleno.Characteristic;
+
+var battery = 23;
 
 /**
 * Constructor for BatteryLevelCharacteristic calls constructor from the parent class Characteristic
@@ -52,40 +56,45 @@ util.inherits(BatteryLevelCharacteristic, Characteristic);
 * This method is called if the master initiates an onReadRequest on the battery level characteristic.
 * We check the os if the application runs on linux, and if so, return the actual battery level
 */
-BatteryLevelCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  // check operating system to be linux
-  if (os.platform() === 'linux') {
-    console.log("Get battery level");
-    // execute child process and get battery level from stdout
-    var bat = executeShellCommand( async function (result){
-      console.log(result);
-      // read data from console
-      // expected output ===>>  98%
-      var battery = parseInt(result);
-      console.log("Inside battery level: " + battery);  
-      return battery;
-      // send data to master
-    });        
-    callback(this.RESULT_SUCCESS, new Buffer([bat]));;
-  } else {
-    // create random value
-    var rand = parseInt(Math.floor(Math.random() * 100) + 1);
-    console.log("Randomized battery level: " + rand);
-    callback(this.RESULT_SUCCESS, new Buffer([rand]));
-  }
-};
-
-var executeShellCommand = async function(callback) {
-  exec("acpi battery | head -n 1 | cut -d ',' -f2", function (error, stdout, stderr) {
-    // error handling
-    if (error instanceof Error) {
-      console.log(error);
-      throw error;
-    } else {  
-      callback(stdout);   
+BatteryLevelCharacteristic.prototype.onReadRequest = async function(offset, callback) {
+ 
+  // asynchronous waterfall principle do async function after the previous finished
+  async.waterfall([
+    function(callback) {
+      // check operating system to be linux
+      if (os.platform() === 'linux') {
+        console.log("Get battery level");
+        // get battery level from os command shell
+        exec("acpi battery | head -n 1 | cut -d ',' -f2", function (error, stdout, stderr) {
+          // Expected output => 95%
+          // error handling
+          if (error instanceof Error) {
+            console.log(error);
+          } else {  
+            // return shell outout to caller
+            console.log("Inside: " + stdout.toString());
+            callback(null, stdout.toString());   
+          }
+        });
+      } else {
+        // other os than linux create random value for battery level
+        var rand = parseInt(Math.floor(Math.random() * 100) + 1);
+        console.log("Randomized battery level: " + rand);
+        callback(null, rand);
+      }
     }
-  });
-}
+      // return value from internal callback are stored in result
+    ], function(error, result) {
+      if(error != null) {
+        console.log(error);
+      } else { 
+      console.log("Inside finished: " + result);
+      battery = result;
+      // send data to master
+      callback(this.RESULT_SUCCESS, new Buffer([battery]));
+      }
+    });   
+};
 
 // export class as BatteryLevelCharacteristic
 module.exports = BatteryLevelCharacteristic;
